598f4c5a52c412d447d9dda0b7d18c54ad9aedc6
Xavier Areas <xareas@gmail.com>
2018-08-21 13:25:14 -0600

    Initial Commit

---
+++ PretyIcons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PretyIcons
+//
+//  Created by Francisco Javier Areas Rios on 8/21/18.
+//  Copyright © 2018 Francisco Javier Areas Rios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
